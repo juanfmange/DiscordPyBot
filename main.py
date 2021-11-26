97c2526ff282b430468e0fce1f5e76d6d41c3ad5
juan mange <juanfmange@gmail.com>
2021-11-25 21:37:28 -0600

    Se arreglo gracias a las modificaciones del flakk

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import discord
 from discord.ext import commands
 import music
 
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
 cogs = [music]
 
-client = commands.Bot(command_prefix='-', intents = discord.Intents.all())
+client = commands.Bot(command_prefix='-', intents = intents )
+
 
 
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
 
-client.run('') #Token
+client.run('')
